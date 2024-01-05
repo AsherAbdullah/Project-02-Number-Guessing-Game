158345fa4a862ba63880197d53123150c392c2b1
AsherAbdullah <155197281+AsherAbdullah@users.noreply.github.com>
2024-01-05 06:38:18 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Write Your Guess B/w 1 to 100: "
+    }
+]);
+const { userGuess } = answer;
+if (userGuess == systemGeneratedNo) {
+    console.log(userGuess, "userGuess", systemGeneratedNo, "SYs");
+    console.log("yaaaaaa! Your answer is correct \n you Win!");
+}
+else {
+    console.log("Please try again Better luck next time ");
+}
